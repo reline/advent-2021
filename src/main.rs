7ae5c15721086f3bd8aa800facf3685a10a3aa9e
Nathan Reline <nathan.reline@gm.com>
2021-12-18 06:44:58 -0700

    Day 7 Part Two

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     match args.day.as_str() {
         "7" => {
             let input = io::split_num_commas(args.path);
-            println!("Part One: {}", day7::partone(&input))
+            println!("Part One: {}", day7::partone(&input));
+            println!("Part Two: {}", day7::parttwo(&input));
         }
         _ => {
             println!("Incomplete day")
